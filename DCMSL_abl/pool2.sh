4b3ad419087c5ac8f511b3ef4292496e5edff432
HanChen-HUST <45957029+HanChen-HUST@users.noreply.github.com>
2024-07-17 02:21:37 -0500

    Update pool2.sh

--- DCMSL_abl/pool2.sh
+++ DCMSL_abl/pool2.sh
@@ -1,5 +1,5 @@
-python train_pooling.py --dataset Ogbn --param local:ogbn.json --device cuda:2 --poolway rand
-python train_pooling.py --dataset Ogbn --param local:ogbn.json --device cuda:2 --poolway rand
-python train_pooling.py --dataset Ogbn --param local:ogbn.json --device cuda:2 --poolway rand
-python train_pooling.py --dataset Ogbn --param local:ogbn.json --device cuda:2 --poolway rand
-python train_pooling.py --dataset Ogbn --param local:ogbn.json --device cuda:2 --poolway rand
+python train.py --dataset Ogbn --param local:ogbn.json --device cuda:2 --poolway rand
+python train.py --dataset Ogbn --param local:ogbn.json --device cuda:2 --poolway rand
+python train.py --dataset Ogbn --param local:ogbn.json --device cuda:2 --poolway rand
+python train.py --dataset Ogbn --param local:ogbn.json --device cuda:2 --poolway rand
+python train.py --dataset Ogbn --param local:ogbn.json --device cuda:2 --poolway rand
